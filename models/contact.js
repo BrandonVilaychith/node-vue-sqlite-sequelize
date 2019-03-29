0b231b1838d830e2705a49eb9b0effffee96d9e6
Brandon Vilaychith <blvilaychith@gmail.com>
2019-03-28 22:46:32 -0500

    update

--- models/contact.js
+++ models/contact.js
@@ -1,13 +1,19 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Contact = sequelize.define('Contact', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {});
-  Contact.associate = function(models) {
-    // associations can be defined here
-  };
-  return Contact;
+	const Contact = sequelize.define(
+		"Contact",
+		{
+			firstName: DataTypes.STRING,
+			lastName: DataTypes.STRING,
+			phone: DataTypes.STRING,
+			email: DataTypes.STRING,
+		},
+		{},
+	);
+	Contact.associate = function(models) {
+		// associations can be defined here
+	};
+	return Contact;
 };
+
+// npx sequelize db:migrate
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const db = require("./models");
 
 const app = express();
 
@@ -7,7 +8,12 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + "/static"));
 
 app.get("/api/contacts", (req, res) => {
-	// TODO
+	return db.Contact.findAll()
+		.then(contacts => res.send(contacts))
+		.catch(err => {
+			console.log("There was an error querying contacts", JSON.stringify(err));
+			return res.send(err);
+		});
 });
 
 app.post("/api/contacts", (req, res) => {
